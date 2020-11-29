b89ac83c9ea523810343f9a43d4575dc8cda0bac
ray_arwindra <wan.muhammad81@ui.ac.id>
2020-11-29 09:44:55 +0700

    Fixed fetch calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Navbar, Container, Form, Button } from 'react-bootstrap';
+import { Navbar, Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import WeatherInfo from './WeatherInfo'
+import cloud from './cloud.png'
 
 function App() {
   const [weather, setWeather] = useState({});
 
-  const emptyData = () => {
+  const [value, setValue] = useState("");
+
+  const [city, setCity] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCity(value);
+  }
+
+  const isEmpty = () => {
     console.log(Object.keys(weather).length);
     console.log(weather);
     return Object.keys(weather).length === 0;  
   }
 
-  useEffect( () => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=tangerang&appid=3e70d8524c9a70b8d20ee49b142ee968&units=metric')
+  const getWeather = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3e70d8524c9a70b8d20ee49b142ee968&units=metric`)
       .then(response => response.json())
-      .then(data => setWeather({data}));
-  }, []);
+      .then(data => setWeather(data));
+    console.log(weather);
+  }
+
+  useEffect( () => {
+    getWeather();
+  }, [city]);
   return (
     <div className="App">
       <Navbar className="navbar" bg="dark" variant="dark">
@@ -26,24 +45,32 @@ function App() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
           </Container>
         </Navbar>
-        <Container className='mt-4'>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Control type="text" placeholder="Weather in your city..." />
-          </Form.Group>
-          <Button variant="primary" type="submit">Submit</Button>
+        <Container className='mt-3'>
+        <Form onSubmit={handleSubmit}>
+          <Row>
+            <Col>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control value={value} onChange={handleChange} type="text" placeholder="Weather in your city..." />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button variant="primary" type="submit">Submit</Button>
+            </Col>
+          </Row>
         </Form>          
-        { emptyData() || weather.cod === 429 ?
+        { isEmpty() || weather.cod !== 200 ?
         <div>
-          <img src="public/cloud.png" alt="Cloud"></img>
+          <img style={{width: '25%', height:'25%'}} src={cloud} alt="Cloud"></img>
           <h3>Please input a search query...</h3>
         </div>
         :
         <div>
-          <WeatherInfo {...weather.data}></WeatherInfo>
+          <WeatherInfo {...weather}></WeatherInfo>
         </div>
         }
+        <p className="mt-2">Made by Rayhan Arwindra</p>
         </Container>
+        
     </div>
   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,28 +8,53 @@ const WeatherInfo = ({
     weather,
     wind,
     name,
+    coord,
 }) => {
     return(
         <div>
-            <Card style={{ width: '100%' }} className="mt-4">
+            <Card style={{ width: '100%' }} className="mt-2">
                 <Card.Body>
-                    <Card.Title>{`${name}, ${sys.country} Weather`}</Card.Title>
+                    <Card.Title><h3>{`${name}, ${sys.country} Weather`}</h3></Card.Title>
                     <Card.Text>
                         <Row>
                             <Col>
-                                <div>Today</div>
+                                <div><h4>Today</h4></div>
                                 <div><h1>{main.temp}</h1></div>
-                                <div>{weather[0].main}</div>
+                                <div><h4>{weather[0].main}</h4></div>
                             </Col>
                             <Col>
-                                <div>Maximum: {main.temp_max}</div>
-                                <div>Minimum: {main.temp_min}</div>
-                                <div>Feels Like: {main.feels_like}</div>
+                                <div><h4>Maximum: {main.temp_max}</h4></div>
+                                <div><h4>Minimum: {main.temp_min}</h4></div>
+                                <div><h4>Feels Like: {main.feels_like}</h4></div>
                             </Col>
                         </Row>
                     </Card.Text>
                 </Card.Body>
             </Card>
+            <Row>
+                <Col>
+                    <Card style={{ width: '100%' }} className="mt-4">
+                        <Card.Body>
+                            <Card.Title><h3>Wind</h3></Card.Title>
+                            <Card.Text>
+                                <div><h4>Speed: {wind.speed}</h4></div>
+                                <div><h4>Degree: {wind.deg}</h4></div>    
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                <Card style={{ width: '100%' }} className="mt-4">
+                        <Card.Body>
+                            <Card.Title><h3>Coordinate</h3></Card.Title>
+                            <Card.Text>
+                                    <div><h4>Longitude: {coord.lon}</h4></div>
+                                    <div><h4>Latitude: {coord.lat}</h4></div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
